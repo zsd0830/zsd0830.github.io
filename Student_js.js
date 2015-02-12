3bb67e494b7c0dbf87b3e9ee734b3b8df6dc5426
zsd0830 <zsd0830@gmail.com>
2015-02-12 20:14:25 +0800

    Create Student_js.js

---
+++ Student_js.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $.getJSON("https://hivelab.org/static/students.json", function(sad){
+      var c=[];
+        for (var i = 0; i < sad.length; i++) {
+            c = $('<tr/>');
+            c.append("<td>" + sad[i].Name + "</td>");
+            c.append("<td>" + sad[i].GPA + "</td>");
+            c.append("<td>" + sad[i].GRE_V + "</td>");
+            c.append("<td>" + sad[i].GRE_Q + "</td>");
+            c.append("<td>" + sad[i].Essay+ "</td>");
+            c.append("<td>" + sad[i].Recom+ "</td>");
+            $('#information_table').append(c); 
+        }
+        })
+    })
